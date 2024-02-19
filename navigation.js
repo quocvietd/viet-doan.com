4e687cdf95322fad1fce09453c86e55e722fd98e
quocvietd <vietdoan.vqd@proton.me>
2024-02-19 16:57:56 +1100

    Dropdown Menu - JS - [Currently not Working]
    - Attempted to created a separate .js file for the dropdown menu. However, it is currently not working, so the JS for the dropdown menu will be an script within the main index.html file

---
+++ navigation.js
@@ -0,0 +1,12 @@
+// const toggleBtn = document.querySelector('.nav-menu');
+// const toggleBtnIcon = document.querySelector('.nav-menu i');
+// const dropDownMenu = document.querySelector('.dropdown-menu');
+
+// toggleBtn.onclick = function() {
+//   dropDownMenu.classList.toggle('open');
+//   const isOpen = dropDownMenu.classList.contains('open');
+  
+//   toggleBtnIcon.className = isOpen
+//     ? "fa-solid fa-xmark"
+//     : "fa-solid fa-bars";
+// };
