a882af3e632523b96584d5aa9e8d5963f32cab2d
vi <vietdoan.vqd@gmail.com>
2024-02-20 14:49:48 +1100

    Element change from Div to Sections
    More Notes:
    - Add extra colors to notes.txt
    - Testing with Active nav links changes color on active [still not working]

--- navigation.js
+++ navigation.js
@@ -10,3 +10,22 @@
 //     ? "fa-solid fa-xmark"
 //     : "fa-solid fa-bars";
 // };
+
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav.nav-bar ul.nav-bookmarks a');
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if(top >= offset && top < offset + height) {
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+//             });
+//         };
+//     });
+// };
